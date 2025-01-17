935bcfbc7ee08677bdcfb3ee458efb93972775f5
Giovanni Nespoli <ggnespoli@gmail.com>
2025-01-17 14:25:28 -0300

    feat cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/header";
+import { CartProvider } from "@/contexts/cart-context";
 import { Metadata } from "next";
 import React, { ReactNode } from "react";
 
@@ -8,9 +9,11 @@ export const metadata: Metadata = {
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }

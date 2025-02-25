4d2d1a55075894704c36a36d2004e9c416a89dd9
Giovanni Nespoli <ggnespoli@gmail.com>
2025-02-25 09:17:10 -0300

    feat: cypress custom commands

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,10 +1,6 @@
 describe("search product", () => {
   it("should be able to search for products", () => {
-    cy.visit("/");
-
-    cy.get("input[name=q]").type("moletom").parent("form").submit();
-
-    cy.location("pathname").should("include", "/search");
+    cy.searchByQuery("moletom");
     cy.location("search").should("include", "q=moletom");
 
     cy.get('a[href^="/product"]').should("exist");
@@ -16,7 +12,6 @@ describe("search product", () => {
     });
 
     cy.visit("/search");
-
     cy.location("pathname").should("equal", "/");
   });
 });
